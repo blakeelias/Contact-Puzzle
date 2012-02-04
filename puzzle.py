997b007dc39cb39ed50ecf1476a11f3829a689ab
Blake Elias <blakeelias@gmail.com>
2012-02-04 00:42:47 -0500

    Use iteration instead of recursion in flip.  Python was complaining about recursion depth.

--- puzzle.py
+++ puzzle.py
@@ -1,25 +1,29 @@
 from random import randint
 
 
-def flip(n, numIterations, hits):
+def flip(n):
+	hits = 0
 	for i in range(int(n)):
 		x = randint(0, div_count - 1)
 		if divs[x] == 1:
 			hits += 1
 			divs[x] = 0
-	if hits > (.99 * div_count):
-		return (numIterations / 10.0);
-	else:
-		return flip(n, numIterations+1, hits)
+	return hits
 
+def iterate(n):
+	hits = 0
+	numIterations = 0
+	while hits <= (.99 * div_count):
+		hits += flip(n)
+		numIterations += 1
+	return (numIterations / 10.0);
 
 if __name__ == "__main__":
 	times = []
 	for attempt in range(10):
 		div_count = 100
-		hits = 0
 		divs = [1] * div_count
-		time = flip(div_count * .01, numIterations = 0, hits = 0)
+		time = iterate(div_count * .01)
 		times.append(time)
 	print(times)
 	print(sum(times) / len(times))
